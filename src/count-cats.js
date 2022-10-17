0f8b0c344098514208415f06ed72f99a648cdb83
Uladimir Hrydzin <hrydzin@gmail.com>
2022-10-17 05:25:04 +0400

    add: solution first three exercises

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+//  countCats([
+//  [0, 1, '^^'],
+//   [0, '^^', 2],
+//   ['^^', 1, 2],
+// ]) ;
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = [].concat(...matrix);
+     
+  let arrayCats = result.filter(function(cat) { 
+      return cat === '^^';
+  });
+ 
+        let catsResult = arrayCats.length;
+      return catsResult;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+// Array.isArray(members);
+// if(!(Array.isArray(members))) {
+//   return false;
+// })
+if(!(Array.isArray(members)) return false;
+
+
+  let result =[];
+  members.forEach(function(item) {
+
+  if (typeof item === 'string'){
+    let i = item.replace(/\s+/g,'');
+     result.push(i);
+  }       
+
+});    
+  
+  return result.flatMap(arr => arr[0].toUpperCase()).sort().join('');
+
+};
 
 module.exports = {
   createDreamTeam
